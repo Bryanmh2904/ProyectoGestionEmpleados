b7044347ef2ffeaf64b74470fa98333b81014cee
Bryanmh2904 <bryanmoreh02@gmail.com>
2025-03-11 12:16:05 -0600

    clase usuario

---
+++ src/proyecto_empleados/Usuario.java
@@ -0,0 +1,26 @@
+package proyecto_empleados;
+
+public class Usuario {
+
+    private String nombre;
+    private String contraseña;
+    private String rol;
+
+    public Usuario(String nombre, String contraseña, String rol) {
+        this.nombre = nombre;
+        this.contraseña = contraseña;
+        this.rol = rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+}
